367f587f2782cc1db2d442f37846b1d5d749ab48
Martin Schaumann <m.schaumann@mscloud.uk>
2024-11-17 16:03:45 +0100

    added i3 config

--- dwmblocks/scripts/bright.sh
+++
@@ -1,1 +0,0 @@
-echo $(($(cat /sys/class/backlight/intel_backlight/brightness) * 100 / $(cat /sys/class/backlight/intel_backlight/max_brightness)))%
--- dwmblocks/scripts/vol.sh
+++
@@ -1,1 +0,0 @@
-amixer get Master | grep -oP '\[\d+%\]' | head -n 1 | tr -d '[]'
